81f857faf8ac93cabbd26b4466b9c618d9d95930
manuel.garcia <manuel.garcia@farfetch.com>
2021-04-15 16:26:29 +0100

    fix2

--- functions/proxy.js
+++ functions/proxy.js
@@ -7,24 +7,14 @@ const API_ENDPOINT = "https://www.farfetch.com/shopping/men/clothing-2/items.asp
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT/*, { headers: { Accept: "application/json" } }*/)
     .then((response) => response.text())
-    .then((data) => {
-      //const dom = parser.parseFromString(data);
+    .then((data) => {      
       const productRegex = /itemID=\"(.*?)\"/mgi;
-      const matches = productRegex.exec(data);
-      //let html = JSON.stringify(matches);
-      let html = data;
+      const matches = [...data.matchAll(productRegex)];
+      let html = '';
       if (matches.length > 0) {        
         html = matches.find(match => match[1].includes(".aspx?storeid="))[0].split("=")[1];
-        //html = matches;
+        
       }
-
-      /*const products = dom.getElementsByAttribute("itemtype", "http://schema.org/Product") || [];
-      
-      if (products.length > 0) {
-        let url = products[0].getAttribute("href");
-        html = url;
-      }*/
-
       return {
       statusCode: 200,
       body: html
