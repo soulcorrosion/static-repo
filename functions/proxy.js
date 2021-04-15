489a3a8e7c482d421128114d44aa20bf51192292
manuel.garcia <manuel.garcia@farfetch.com>
2021-04-15 13:15:30 +0100

    html url

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,13 +1,13 @@
 const fetch = require("node-fetch");
 
-const API_ENDPOINT = "https://icanhazdadjoke.com/";
+const API_ENDPOINT = "https://www.farfetch.com/shopping/men/clothing-2/items.aspx";
 
 exports.handler = async (event, context) => {
-  return fetch(API_ENDPOINT, { headers: { Accept: "application/json" } })
-    .then((response) => response.json())
+  return fetch(API_ENDPOINT/*, { headers: { Accept: "application/json" } }*/)
+    .then((response) => response.text())
     .then((data) => ({
       statusCode: 200,
-      body: data.joke,
+      body: data,
     }))
     .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
