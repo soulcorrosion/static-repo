a30af679af8b21ee901796dcdf1cde47172d051f
manuel.garcia <manuel.garcia@farfetch.com>
2021-04-15 15:28:01 +0100

    test

--- functions/proxy.js
+++ functions/proxy.js
@@ -11,10 +11,10 @@ exports.handler = async (event, context) => {
       //const dom = parser.parseFromString(data);
       const productRegex = /itemID=\"(.*?)\"/gi;
       const matches = productRegex.exec(data);
-      let html = '';
-      if (matches.length > 0) {
+      let html = matches[0];
+      /*if (matches.length > 0) {
         html = matches[0].split("=")[1];
-      }
+      }*/
 
       /*const products = dom.getElementsByAttribute("itemtype", "http://schema.org/Product") || [];
       
