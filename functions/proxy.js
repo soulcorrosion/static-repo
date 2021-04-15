78f4681078b6302a5122079c6256ce0de91ece0f
manuel.garcia <manuel.garcia@farfetch.com>
2021-04-15 14:39:30 +0100

    Added dom parser

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,13 +1,24 @@
 const fetch = require("node-fetch");
+const DomParser = require('dom-parser');
 
+const parser = new DomParser();
 const API_ENDPOINT = "https://www.farfetch.com/shopping/men/clothing-2/items.aspx";
 
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT/*, { headers: { Accept: "application/json" } }*/)
     .then((response) => response.text())
-    .then((data) => ({
+    .then((data) => {
+      const dom = parser.parseFromString(data);
+      const products = dom.getElementsByAttribute("itemtype", "http://schema.org/Product") || [];
+      let html = '';
+      if (products.length > 0) {
+        let url = products[0].getAttribute("href");
+        html = url;
+      }
+      return {
       statusCode: 200,
-      body: data,
-    }))
+      body: html
+    };
+  })
     .catch((error) => ({ statusCode: 422, body: String(error) }));
 };
