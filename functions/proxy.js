9398d9d610ed4597cc54527512ce01787b2ba0fc
manuel.garcia <manuel.garcia@farfetch.com>
2021-04-15 16:29:29 +0100

    test link

--- functions/proxy.js
+++ functions/proxy.js
@@ -2,7 +2,7 @@ const fetch = require("node-fetch");
 const DomParser = require('dom-parser');
 
 const parser = new DomParser();
-const API_ENDPOINT = "https://www.farfetch.com/shopping/men/clothing-2/items.aspx";
+const API_ENDPOINT = "https://www.farfetch.com/uk/shopping/men/clothing-2/items.aspx";
 
 exports.handler = async (event, context) => {
   return fetch(API_ENDPOINT/*, { headers: { Accept: "application/json" } }*/)
@@ -12,7 +12,7 @@ exports.handler = async (event, context) => {
       const matches = [...data.matchAll(productRegex)];
       let html = '';
       if (matches.length > 0) {        
-        html = matches.find(match => match[1].includes(".aspx?storeid="))[0].split("=")[1];
+        html = `<a href="https://www.farfetch.com/uk/${matches.find(match => match[1].includes(".aspx?storeid="))[0].split('="')[1]}"Click Me</a>`;
         
       }
       return {
